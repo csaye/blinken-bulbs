fe53634370452a0759a3370a6db009b678c90e76
Cooper Saye <coopersaye@gmail.com>
2022-06-22 01:09:34 -0700

    Update primary program.

--- primary.js
+++ primary.js
@@ -6,4 +6,13 @@ new Blinken({
     lights[i].a = 1;
     lights[i].rgb(0, 0, 1);
   }
+  let i = 0;
+  return () => {
+    const j = Math.floor(i / 100) % 3;
+    if (j === 0) lights[i % 100].rgb(1, 0, 0);
+    else if (j === 1) lights[i % 100].rgb(1, 1, 0);
+    else lights[i % 100].rgb(0, 0, 1);
+    i += 1;
+    return 40;
+  };
 });
