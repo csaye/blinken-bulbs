e81804a8d30625e606ec83e5c2cdab3bae2f0649
Cooper Saye <coopersaye@gmail.com>
2022-06-22 01:11:01 -0700

    Update rainbow program.

--- rainbow.js
+++ rainbow.js
@@ -6,4 +6,21 @@ new Blinken({
     lights[i].a = 1;
     lights[i].rgb(1, 1, 1);
   }
+  let i = 0;
+  return () => {
+    for (let j = 0; j < 100; j++) {
+      lights[(i + 90) % 100].rgb(1, 0, 0);
+      lights[(i + 80) % 100].rgb(1, 0.5, 0);
+      lights[(i + 70) % 100].rgb(1, 1, 0);
+      lights[(i + 60) % 100].rgb(0, 1, 0);
+      lights[(i + 50) % 100].rgb(0, 0, 1);
+      lights[(i + 40) % 100].rgb(0.5, 0, 1);
+      lights[(i + 30) % 100].rgb(1, 0, 0.5);
+      lights[(i + 20) % 100].rgb(0.5, 0.5, 0.5);
+      lights[(i + 10) % 100].rgb(0, 0, 0);
+      lights[(i +  0) % 100].rgb(1, 1, 1);
+    }
+    i += 1;
+    return 40;
+  };
 });
